fa56dccab43f21626bffd459ea1bb6c4f2c5b97c
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-11 13:20:35 -0700

    added findSteps method

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,30 +1,63 @@
-const db = require('../data/db-config.js');
+const db = require("../data/db-config.js");
 
 function find() {
-    return db('schemes')
+  return db("schemes")
     .select()
     .then(schemes => {
-        return schemes;
-    })
+      return schemes;
+    });
 }
 
 function findById(id) {
-    return db('schemes')
+  return db("schemes")
     .select()
     .where({ id: id })
     .first()
 
     .then(res => {
+      if (res) {
+        return res;
+      } else {
+        return null;
+      }
+    });
+}
+
+function findSteps(id) {
+    return db('steps')
+    .select('scheme_name','step_number', 'instructions').from('steps')
+    .join('schemes', 'schemes.id', '=', 'steps.scheme_id')
+    .where({ scheme_id: id })
+
+    .then(res => {
         if (res) {
-            return res;
+          return res;
         } else {
-            return null;
+          return null;
         }
-    })
+      });
+}
+
+function add(scheme) {
 
 }
+
+
+function update(changes, id) {
+
+}
+
+function remove(id) {
+
+}
+
+
 
 module.exports = {
-    find,
-    findById,
-}
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+};
